7b656f89e3f3ca574dc8b0e93bee9175ef5431c5
rkaaudgus <rkaaudgus@naver.com>
2019-07-01 17:17:42 +0900

    이미지 삭제 밒 병경

--- myAdd/ViewController.swift
+++ myAdd/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("안녕하쇼~")
+        //ㅇㅇ
+        print("안녕핫!")
     }
 
 
